c31c894bcc98ad2e813de340e9be1cfbce6ef6c8
matiaschanquia <matiaschanquianahuel@gmail.com>
2023-01-10 20:33:22 -0300

    news products

--- src/data.js
+++ src/data.js
@@ -25,9 +25,27 @@ const products = [
     },
     {
         id: 5,
-        name: "Galletas Oreo ",
+        name: "Galletas Oreo",
         precio: 208,
         image: "https://maxiconsumo.com/media/catalog/product/cache/8313a15b471f948db4d9d07d4a9f04a2/2/0/20095.jpg",
+    },
+    {
+        id: 6,
+        name: "Azúcar Ledesma",
+        precio: 260,
+        image: "https://masonlineprod.vtexassets.com/arquivos/ids/246657-1200-auto?v=637886037932600000&width=1200&height=auto&aspect=true",
+    },
+    {
+        id: 7,
+        name: "Alfajor Terrabusi",
+        precio: 82,
+        image: "https://masonlineprod.vtexassets.com/arquivos/ids/204540-1200-auto?v=637850725828000000&width=1200&height=auto&aspect=true",
+    },
+    {
+        id: 8,
+        name: "Coca Cola 2.25l",
+        precio: 446,
+        image: "https://maxiconsumo.com/media/catalog/product/cache/8313a15b471f948db4d9d07d4a9f04a2/2/6/26836.jpg",
     },
 ]
 
