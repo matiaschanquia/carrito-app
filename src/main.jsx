ecc25e1509bc9d4a18c112e4e39353fb738fd068
matiaschanquia <matiaschanquianahuel@gmail.com>
2023-01-10 15:46:31 -0300

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./styles.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
