489e63c53e5a60fe69f78edfdb4d9357f741ebd1
matiaschanquia <matiaschanquianahuel@gmail.com>
2023-01-12 19:37:57 -0300

    close modal click overside

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,14 @@
 
 
 const Modal = ({ children, isOpen, modalClose }) => {
+
+    const handleClickContentModal = (e) => {
+        e.stopPropagation();
+    }
+
     return(
-        <article className={isOpen ? "container-modal is-open" : "container-modal"}>
-            <div className="modal-content">
+        <article className={isOpen ? "container-modal is-open" : "container-modal"} onClick={modalClose}>
+            <div className="modal-content" onClick={handleClickContentModal}>
                 {children}
                 <button onClick={modalClose}>Aceptar</button>
             </div>
